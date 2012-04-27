618d844dd4149284e8cb3c6a148e6c8fd90bdc39
machv <vladimir@mach.im>
2012-04-27 10:54:11 +0000

    ukol2 base
    git-svn-id: http://svn.mysh.cz/svn/dpp/trunk/ukol2@34 1226bc4f-404c-4173-887a-3a9a57dac752

---
+++ Arpar/Parameter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arpar
+{
+    public enum TypParametru { Short, Long }
+
+    /// <summary>
+    /// Typy parametrů
+    /// </summary>
+    public enum ParameterRequirements { Mandatory, Optional, Denied }
+
+    /// <summary>
+    /// Třída pro jeden parametr
+    /// TODO: lépe pojmenovat T -> podívat se do specifikace ms
+    /// </summary>
+    public class Option
+    {
+        //public string Name { get; set; }
+        public TypParametru Type { get; set; }
+        public string Description { get; set; }
+        public bool IsMandatory { get; set; }
+        public ParameterRequirements Parameter { get; set; }
+        public object Value { get; internal set; }
+        public List<string> Names { get; set; }
+        public Option(string name)
+        {
+            Names = new List<string>();
+            Names.Add(name);
+
+            //throw new ArgumentException
+        }
+
+        public void AddAlias(params string[] names)
+        {
+            Names.Add(names);
+        }
+
+        //TODO
+        public override bool Equals(object obj)
+        {
+            //if(obj typeof Option)
+            //{
+            //}
+            return base.Equals(obj);
+        }
+    }
+}
---
+++ TestApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Arpar;
+
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options o = new Options();
+
+
+
+            //o.Parse(args);
+
+            Option<string> opt = new Option<string> { Type = Arpar.TypParametru.Long, IsMandatory = true };
+            //opt.setConstraints(...)
+        }
+    }
+}
