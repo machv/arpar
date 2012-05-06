7593f9996bda7b6692e6fa395e859d9d72184af9
skodad <skodad>
2012-05-06 16:55:48 +0000

    Dokumentace k CommandLineArgumentType
    git-svn-id: http://svn.mysh.cz/svn/dpp/trunk/ukol2@74 1226bc4f-404c-4173-887a-3a9a57dac752

--- Arpar/Types.cs
+++ Arpar/Types.cs
@@ -27,39 +27,41 @@ namespace Arpar
     public enum ParameterRequirements
     {
         /// <summary>
-        /// 
+        /// Mandatory value.
         /// </summary>
         Mandatory,
 
         /// <summary>
-        /// 
+        /// Optional value.
         /// </summary>
         Optional,
 
         /// <summary>
-        /// 
+        /// Denied value.
         /// </summary>
         Denied
     }
 
-    //TODO: dokumentace prosím
     /// <summary>
-    /// 
+    /// Type of the argument from command line.
     /// </summary>
     public enum CommandLineArgumentType
     {
         /// <summary>
-        /// 
+        /// Argument in form with short or long prefix.
+        /// Example: -v --version
         /// </summary>
         Defined,
 
         /// <summary>
-        /// 
+        /// Delimiter of common arguments.
+        /// Example: --
         /// </summary>
         Splitter,
 
         /// <summary>
-        /// 
+        /// Common argument.
+        /// Is not prefixed by "-" neither "--" and is not equal to "--".
         /// </summary>
         Common
     }
