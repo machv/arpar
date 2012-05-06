a5099f8b46db3726a9bf350807722425ef6c9394
skodad <skodad>
2012-05-06 09:32:55 +0000

    Testing cases.
    git-svn-id: http://svn.mysh.cz/svn/dpp/trunk/ukol2@47 1226bc4f-404c-4173-887a-3a9a57dac752

---
+++ TestApplication/BadSettings1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Arpar;
+
+namespace TestApplication
+{
+    class BadSettings1
+    {
+        [Argument("val", ArgumentType.Short, LowBound=0, HighBound=5)]
+        public int value = 0;
+    }
+}
---
+++ TestApplication/BadSettings2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Arpar;
+
+namespace TestApplication
+{
+    class BadSettings2
+    {
+        [Argument("val", ArgumentType.Short, ListOfString = new String[]{"ALPHA", "BETA", "GAMA"})]
+        public string value = "ALPHA";
+    }
+}
